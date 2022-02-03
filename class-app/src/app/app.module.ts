4c9fcc0ecab6b7039faa42c1dce5609aa0614d43
morgnism <morgan@border.dev>
2022-02-02 18:13:09 -0700

    week3 solution

--- class-app/src/app/app.module.ts
+++ class-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -5,11 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PizzaAppComponent } from './pizza-app/pizza-app.component';
+import { NavBarComponent } from './shared/components/nav-bar.component';
 import { SizePipe } from './shared/pipes/size.pipe';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PizzaAppComponent, SizePipe],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    PizzaAppComponent,
+    SizePipe,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- class-app/src/app/home/home.component.ts
+++ class-app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PizzasService } from '../shared/services/pizzas.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  title = 'home!';
+  readonly pizzas$ = this.pizzasService.getPizzaPresets();
+
+  constructor(private pizzasService: PizzasService) {}
 }
