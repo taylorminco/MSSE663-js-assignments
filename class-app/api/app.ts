a459d4dc12fabaf709c3bae3ccf76ddff7e8c43d
morgnism <morgan@border.dev>
2022-02-23 18:04:56 -0700

    added week5 solution

--- class-app/api/app.ts
+++ class-app/api/app.ts
@@ -2,10 +2,22 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { PizzaService } from './src/pizzas.service';
+import mongoose from 'mongoose';
 
 // Setup
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// mongoose instance connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/pizzasdb');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+  console.log('Connection Successful!');
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,23 +29,18 @@ app.use(cookieParser());
 
 const pizzaService = new PizzaService();
 
-app.get('/api/pizzas', (req, res) => {
-  const pizzas = pizzaService.getCreatedPizzas();
+// app.get('/api/pizzas', (req, res) => {
+//   const pizzas = pizzaService.getCreatedPizzas(req, res);
 
-  res.send({
-    msg: 'Found pizzas',
-    pizzas,
-  });
-});
+//   res.send({
+//     msg: 'Found pizzas',
+//     pizzas,
+//   });
+// });
 
-app.get('/api/pizzas/presets', (req, res) => {
-  const pizzas = pizzaService.getPizzaPresets();
-
-  res.send({
-    msg: 'Found pizza presets',
-    pizzas,
-  });
-});
+app.post('/api/pizzas', pizzaService.createPizza);
+app.get('/api/pizzas/presets', pizzaService.getPizzaPresets);
+app.get('/api/pizzas/presets/:id', pizzaService.getPizzaPreset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
--- class-app/api/src/pizzas.service.ts
+++ class-app/api/src/pizzas.service.ts
@@ -1,25 +1,58 @@
+import { Handler } from 'express';
 import { PizzaEntity } from '../lib/api-interfaces';
-import { PIZZAS } from '../lib/pizza';
+import { Pizzas } from './pizzas.model';
 
 export class PizzaService {
-  private readonly pizzaPresets: PizzaEntity[] = PIZZAS;
-  private readonly pizzas: PizzaEntity[] = [];
+  getPizzaPresets: Handler = (req, res) => {
+    Pizzas.find({}, (err: any, data: PizzaEntity[]) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.send({
+        msg: 'Found pizza presets',
+        pizzas: data,
+      });
+    });
+  };
 
-  getPizzaPresets(): PizzaEntity[] {
-    return this.pizzaPresets;
-  }
+  getPizzaPreset: Handler = (req, res) => {
+    Pizzas.findById(req.params.id, (err: any, data: PizzaEntity) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.send({
+        msg: 'Found pizza preset',
+        pizza: data,
+      });
+    });
+  };
 
-  getCreatedPizzas(): PizzaEntity[] {
-    return this.pizzas;
-  }
+  // getCreatedPizzas(): PizzaEntity[] {
+  //   return this.pizzas;
+  // }
 
-  getCreatedPizza(id: string): PizzaEntity {
-    const pizza = this.pizzas.find((pizza) => pizza.id === id);
+  // getCreatedPizza(id: string): PizzaEntity {
+  //   const pizza = this.pizzas.find((pizza) => pizza.id === id);
 
-    if (!pizza) {
-      throw new Error('Pizza not found');
-    }
+  //   if (!pizza) {
+  //     throw new Error('Pizza not found');
+  //   }
 
-    return pizza;
-  }
+  //   return pizza;
+  // }
+
+  createPizza: Handler = (req, res) => {
+    const { pizzas } = req.body;
+    const newPizzas = new Pizzas(pizzas);
+
+    newPizzas.save((err: any, data: any) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.send({
+        msg: 'Created pizzas',
+        pizzas: [data],
+      });
+    });
+  };
 }
